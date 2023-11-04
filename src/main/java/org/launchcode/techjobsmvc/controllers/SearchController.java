a9b2462b24d41fac33f8f0fdf58f36e2c2d0c8a7
Jessica <jlknobeloch@gmail.com>
2023-11-03 21:55:09 -0500

    completed displaySearchResults method and updated fragments.html and search.html files

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
 import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,20 +28,25 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @PostMapping(value = "/search/results")
+    @PostMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
-        // pass ListController.columnChoices as existing search handler does
-        model.addAttribute("columns", columnChoices);
         // store results in a jobs ArrayList
-        ArrayList<Job> jobs = new ArrayList<>();
-        if (searchTerm.equals("all") || searchTerm == null) {
+        ArrayList<Job> jobs;
+
+
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")) {
             // if user enters all or search box blank, call findAll() method from JobData
-            JobData.findAll();
-            put.jobs("")
+            jobs = JobData.findAll();
+
         } else {
             // otherwise send search information to findByColumnAndValue
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
-        return "results";
+        // pass ListController.columnChoices as existing search handler does
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("jobs", jobs);
+        return "search";
     }
 
 }
